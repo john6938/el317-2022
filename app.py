88427f915c681b2ae0e8a3f957323ceaa948fc23
s1280124 <s1280124@u-aizu.ac.jp>
2022-07-27 15:58:28 +0900

    _

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+import nltk
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    text = 'Is he human?'
+    try:
+      morph = nltk.word_tokenize(text)
+      pos = nltk.pos_tag(morph)
+    except:
+      nltk.download('punkt')
+      nltk.download('averaged_perceptron_tagger')
     return render_template("index.html")
 
 if __name__ == '__main__':
