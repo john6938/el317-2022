e5e12602ed18b00b26d02c363a1c621f7ed7cf31
s1280124 <s1280124@u-aizu.ac.jp>
2022-07-27 15:46:52 +0900

    _

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template, request
-from .modules.eng import get_result
-
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return 'hello, world'
 
 if __name__ == '__main__':
     app.run()
